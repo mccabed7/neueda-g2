825a3dc06d6279e40da4e4ec15ad850cb0beaa7f
Declan <mccabed7@tcd.ie>
2024-06-24 10:23:45 +0100

    Added user Repository

--- Backend/demo/src/main/java/com/example/demo/repository/UserRepository.java
+++ Backend/demo/src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,4 +1,21 @@
 package com.example.demo.repository;
+import java.util.ArrayList;
+import com.example.demo.domain.User;
 
 public class UserRepository {
+    private ArrayList<User> userList;
+
+    public UserRepository(){
+        userList = new ArrayList<User>();
+    }
+
+    //Returns User object, or null if not found
+    public User findByName(String userName){
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).getUsername().equals(userName)){
+                return userList.get(i);
+            }
+        }
+        return null;
+    }
 }
