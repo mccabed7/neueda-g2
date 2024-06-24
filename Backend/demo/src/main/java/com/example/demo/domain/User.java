6b503c556334e17d83cd7adc4839e4abe2c971b4
Colm Livingstone <colmlivingstone@gmail.com>
2024-06-24 10:05:33 +0100

    Added User Domain

--- Backend/demo/src/main/java/com/example/demo/domain/User.java
+++ Backend/demo/src/main/java/com/example/demo/domain/User.java
@@ -1,4 +1,44 @@
 package com.example.demo.domain;
 
+import java.util.Objects;
+
 public class User {
+    private String username;
+    private String password;
+    private double userBalance;
+
+
+    public User(String username, String password, double userBalance ) {
+        this.username = username;
+        this.password = password;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getUserBalance() {
+        return userBalance;
+    }
+
+    public void setUserBalance(double userBalance) {
+        this.userBalance = userBalance;
+    }
+    @Override
+    public String toString() {
+        return "User{" + "username='" + username + '\'' + ", password='" + password + '\'' + ", bankBalance=" + userBalance + '}';
+    }
 }
