b4e15822db203d7633b371a750a92c8807d83a57
Kayee Liu <kasterune@gmail.com>
2024-06-24 22:00:22 +0100

    added balance page

---
+++ static/js/balance.js
@@ -0,0 +1,16 @@
+function displayUserDetails()
+{
+    const url = window.location.search;
+    params = new URLSearchParams(url);
+    username = params.get('username')
+    document.getElementById("balance").innerHTML = "User: " + username;
+
+    const req = new XMLHttpRequest();
+    const attemptURL = "http://localhost:8080/api/users?username=" + username;
+    req.open("GET", attemptURL, false)
+    req.send();
+    if (req.status == 200)
+    {
+        document.getElementById("balance").innerHTML = "User: " + username + " with balance: £" + req.responseText;
+    }
+}
