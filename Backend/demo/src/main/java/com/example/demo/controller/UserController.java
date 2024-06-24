092f9ff03c12d91d374ccbb2e34c6b00a3996bec
Kayee Liu <kasterune@gmail.com>
2024-06-24 10:48:07 +0100

    adding POST and GET controller methods

--- Backend/demo/src/main/java/com/example/demo/controller/UserController.java
+++ Backend/demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,46 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.UserService;
+import com.example.demo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+// Disable CORS policy
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
 public class UserController {
+    @Autowired
+    private UserService service;
+
+    /* ENDPOINTS */
+
+    /* POST */
+    @PostMapping("/addUser")
+    public String addNewUser(@RequestBody User newUser) {
+        if (service.addUsers(newUser)) {
+            return "Registered";
+        }
+        return "New user registration failed";
+    }
+
+    /* GET */
+
+    @GetMapping("/users/{username}")
+    public String getUserID(@PathVariable User user) {
+        // Check User ID using service method
+
+        // Return ID if found, else not found message
+
+        return "Placeholder";
+    }
+
+    @GetMapping("/users/{username}/balance")
+    public float getUserBalance(@PathVariable User user) {
+        // Check User ID
+
+        return service.getBalance(user);
+    }
 }
